f6024e831edb222ca71e1c10f8236a8cda8d9588
Wxhhaha <562671024@qq.com>
2025-09-25 10:54:51 +0800

    Initial commit

---
+++ mlkit-scanner/src/main/java/com/xiaoguang/widget/mlkitscanner/callback/CameraAnalyserCallback.java
@@ -0,0 +1,17 @@
+package com.xiaoguang.widget.mlkitscanner.callback;
+
+import android.graphics.Bitmap;
+
+import com.google.mlkit.vision.barcode.common.Barcode;
+
+import java.util.List;
+
+
+/**
+ * @author : maning
+ * @date : 8/19/21
+ * @desc : 扫码分析结果回调
+ */
+public interface CameraAnalyserCallback {
+    void onSuccess(Bitmap bitmap, List<Barcode> barcodes);
+}
